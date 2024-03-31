6d747c77896b8f88856a5547fb7a163e087fc227
Moon-ByeongHak <nate_id@naver.com>
2024-04-01 08:55:50 +0900

    Initial commit

---
+++ src/main/java/com/ttodampartners/ttodamttodam/TtodamTtodamApplication.java
@@ -0,0 +1,13 @@
+package com.ttodampartners.ttodamttodam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtodamTtodamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtodamTtodamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttodampartners/ttodamttodam/TtodamTtodamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttodampartners.ttodamttodam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtodamTtodamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
