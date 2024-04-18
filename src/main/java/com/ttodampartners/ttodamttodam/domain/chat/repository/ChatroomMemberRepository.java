c60b62a39a5ab5841c1ad72ffec89da010704b2d
Ji-eun Yang <jidak1165@gmail.com>
2024-04-18 16:02:57 +0900

    [#13] feat: 채팅방 입장 API 구현

--- src/main/java/com/ttodampartners/ttodamttodam/domain/chat/repository/ChatroomMemberRepository.java
+++ src/main/java/com/ttodampartners/ttodamttodam/domain/chat/repository/ChatroomMemberRepository.java
@@ -11,4 +11,5 @@ import java.util.List;
 @Repository
 public interface ChatroomMemberRepository extends JpaRepository<ChatroomMemberEntity, Long> {
     List<ChatroomMemberEntity> findAllByUserEntity(UserEntity user);
+    List<ChatroomMemberEntity> findAllByChatroomEntity(ChatroomEntity chatroom);
 }
