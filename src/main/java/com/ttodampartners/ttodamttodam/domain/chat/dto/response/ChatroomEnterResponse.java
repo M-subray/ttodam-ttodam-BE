07a61cfb98d12c9449cba1aad453fe9f09e772e1
Ji-eun Yang <jidak1165@gmail.com>
2024-04-18 16:02:57 +0900

    [#13] feat: 채팅방 입장 API 구현

---
+++ src/main/java/com/ttodampartners/ttodamttodam/domain/chat/dto/response/ChatroomEnterResponse.java
@@ -0,0 +1,16 @@
+package com.ttodampartners.ttodamttodam.domain.chat.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class ChatroomEnterResponse {
+    private List<ChatroomProfileResponse> profiles;
+}
