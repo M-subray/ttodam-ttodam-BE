8a82a5ffd733db67d27d916fb6bbafbfe737c483
Ji-eun Yang <jidak1165@gmail.com>
2024-04-20 00:04:37 +0900

    [#22] refactor: SchedulingConfig 파일 위치 변경

--- src/main/java/com/ttodampartners/ttodamttodam/global/config/scheduler/SchedulingConfig.java
+++ src/main/java/com/ttodampartners/ttodamttodam/global/config/scheduler/SchedulingConfig.java
@@ -1,4 +1,4 @@
-package com.ttodampartners.ttodamttodam.global.config.db;
+package com.ttodampartners.ttodamttodam.global.config.scheduler;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
