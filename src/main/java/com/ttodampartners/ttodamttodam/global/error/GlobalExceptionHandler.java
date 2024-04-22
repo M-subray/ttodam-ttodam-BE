38cd1869b3ecb9989b1bc45e9f5b729ef27f4768
Ji-eun Yang <jidak1165@gmail.com>
2024-04-22 13:50:20 +0900

    [#27] fix: GlobalExceptionHandler 주석 수정

--- src/main/java/com/ttodampartners/ttodamttodam/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/ttodampartners/ttodamttodam/global/error/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package com.ttodampartners.ttodamttodam.global.error;
 
-//import com.ttodampartners.ttodamttodam.domain.user.exception.AwsException;
-//import com.ttodampartners.ttodamttodam.domain.user.exception.CoordinateException;
-//import com.ttodampartners.ttodamttodam.infra.email.exception.MailException;
-//import com.ttodampartners.ttodamttodam.domain.user.exception.UserException;
+import com.ttodampartners.ttodamttodam.domain.user.exception.AwsException;
+import com.ttodampartners.ttodamttodam.domain.user.exception.CoordinateException;
+import com.ttodampartners.ttodamttodam.infra.email.exception.MailException;
+import com.ttodampartners.ttodamttodam.domain.user.exception.UserException;
 import com.ttodampartners.ttodamttodam.domain.chat.dto.ChatExceptionResponse;
 import com.ttodampartners.ttodamttodam.domain.chat.exception.ChatroomException;
 import com.ttodampartners.ttodamttodam.domain.chat.exception.ChatroomStringException;
@@ -18,29 +18,29 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-//  @ExceptionHandler(UserException.class)
-//  public ResponseEntity<String> userExceptionHandle(UserException e) {
-//    log.error("에러코드: {}, 에러 메시지: {}", e.getErrorCode(), e.getErrorMessage());
-//    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getErrorMessage());
-//  }
-//
-//  @ExceptionHandler(MailException.class)
-//  public ResponseEntity<String> mailExceptionHandle(MailException e) {
-//    log.error("에러코드: {}, 에러 메시지: {}", e.getErrorCode(), e.getErrorMessage());
-//    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getErrorMessage());
-//  }
-//
-//  @ExceptionHandler(AwsException.class)
-//  public ResponseEntity<String> AwsExceptionHandle(AwsException e) {
-//    log.error("에러코드: {}, 에러 메시지: {}", e.getErrorCode(), e.getErrorMessage());
-//    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getErrorMessage());
-//  }
-//
-//  @ExceptionHandler(CoordinateException.class)
-//  public ResponseEntity<String> CoordinateExceptionHandle(CoordinateException e) {
-//    log.error("에러코드: {}, 에러 메시지: {}", e.getErrorCode(), e.getErrorMessage());
-//    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getErrorMessage());
-//  }
+  @ExceptionHandler(UserException.class)
+  public ResponseEntity<String> userExceptionHandle(UserException e) {
+    log.error("에러코드: {}, 에러 메시지: {}", e.getErrorCode(), e.getErrorMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getErrorMessage());
+  }
+
+  @ExceptionHandler(MailException.class)
+  public ResponseEntity<String> mailExceptionHandle(MailException e) {
+    log.error("에러코드: {}, 에러 메시지: {}", e.getErrorCode(), e.getErrorMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getErrorMessage());
+  }
+
+  @ExceptionHandler(AwsException.class)
+  public ResponseEntity<String> AwsExceptionHandle(AwsException e) {
+    log.error("에러코드: {}, 에러 메시지: {}", e.getErrorCode(), e.getErrorMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getErrorMessage());
+  }
+
+  @ExceptionHandler(CoordinateException.class)
+  public ResponseEntity<String> CoordinateExceptionHandle(CoordinateException e) {
+    log.error("에러코드: {}, 에러 메시지: {}", e.getErrorCode(), e.getErrorMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getErrorMessage());
+  }
 
   @ExceptionHandler(ChatroomException.class)
   public ResponseEntity<ChatExceptionResponse> ChatroomExceptionHandler(ChatroomException e) {
